15b4b2689c99ea186ac91340be8362e6970c55eb
Esmeralda Hoscoy <esmeralda@websprout.org>
2016-03-13 14:04:19 -0400

    dannas edits

--- story.rb
+++ story.rb
@@ -1,15 +1,11 @@
-require 'pry'
+def display_story (x,y)
+	puts "I rate the movie #{x} as a #{y} out of 100"
+end
 
-#write a method that accepts two parameters: title and rating
-  #the method should print the story title and the rating out of ten
+puts "What is the title of the movie"
+user_title = gets.strip
 
-
-#show the user a message asking them for a title
-
-#store the input in a variable
-
-#show the user a message asking for their rating of the movie out of 100
-
-# store the input in a variable
+puts "What do you rate this movie (out of 100)"
+user_rating = gets.strip
 
 display_story(user_title, user_rating)
